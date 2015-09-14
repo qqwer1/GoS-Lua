ea329c9a091d56d3cbd4b0b64e8bd1cfba1f4389
qqwer1 <cfz8points@yahoo.de>
2015-09-14 20:14:01 +0200

    Update RecallUlt.lua

--- RecallUlt.lua
+++ RecallUlt.lua
@@ -1,11 +1,11 @@
-require('Inspired')
+require('InspiredNew')
 
-mainMenu = Menu("RecallUlt", "RecallUlt1")
-mainMenu:SubMenu("recallult", "Recall Ult (Beta)")
-mainMenu.recallult:Boolean("recallult", "Recall Ult (Beta)", true)
-mainMenu.recallult:Boolean("recalldraw", "Draw Ult Pos", true)
-mainMenu:SubMenu("baseult", "BaseUlt")
-mainMenu.baseult:Boolean("baseult", "Base Ult", true)
+recallMenu = Menu("RecallUlt", "RecallUlt1")
+recallMenu:SubMenu("recallult", "Recall Ult (Beta)")
+recallMenu.recallult:Boolean("recallult1", "Recall Ult (Beta)", true)
+recallMenu.recallult:Boolean("recalldraw", "Draw Ult Pos", true)
+recallMenu:SubMenu("baseult", "BaseUlt")
+recallMenu.baseult:Boolean("baseult1", "Base Ult", true)
 
 
 PrintChat("Noddy | RecallUlt loaded.")
@@ -14,8 +14,6 @@ global_ticks = 0
 global_ticks1 = 0
 TickerStart = 0
 
--- local target = GetCurrentTarget()
--- local enemyPos2 = GetOrigin(target)
 local myHero = GetMyHero()
 
 -- Team
@@ -57,7 +55,6 @@ elseif GetObjectName(myHero) == "Jinx" then
 skillrange = 50000
 speedChamp = 2000
 speedSpawn = (GoS:GetDistance(enemyBasePos) / (1 + (GoS:GetDistance(enemyBasePos)-1700)/2600))
--- speedSpawn = (GoS:GetDistance(enemyBasePos) / (1 + (GoS:GetDistance(enemyBasePos)-1700)/2600))
 delay = 600
 colision = true
 dmg = function(target) return GoS:CalcDamage(myHero, target, ((GetMaxHP(target)-GetCurrentHP(target))*(0.2+0.05*GetCastLevel(myHero, _R))) + 100*GetCastLevel(myHero,_R) + 150 + GetBonusDmg(myHero)) end
@@ -92,7 +89,7 @@ OnLoop (function (myHero)
 
 Ticker1 = GetTickCount()
 
-if EndPos ~= nil and mainMenu.recallult.recalldraw:Value() then
+if EndPos ~= nil and recallMenu.recallult.recalldraw:Value() then
 DrawCircle(EndPos,25,1,100,0xff00ffff)
 	GoS:DelayAction(function ()	
 		EndPos = nil	
@@ -100,9 +97,7 @@ DrawCircle(EndPos,25,1,100,0xff00ffff)
 end
 
 for i,enemy in pairs(GoS:GetEnemyHeroes()) do	
-	if CanUseSpell(myHero,_R) == READY and mainMenu.recallult.recallult:Value() and GoS:ValidTarget(enemy) and GetCurrentHP(enemy) < dmg(enemy) then
-	-- if CanUseSpell(myHero,_R) == READY and mainMenu.recallult.recallult:Value() and GoS:ValidTarget(enemy) then
-	-- if mainMenu.recallult.recallult:Value() and GoS:ValidTarget(enemy) then
+	if CanUseSpell(myHero,_R) == READY and recallMenu.recallult.recallult1:Value() and GoS:ValidTarget(enemy) and GetCurrentHP(enemy) < dmg(enemy) then
 
 		Ticker = GetTickCount()
 		
@@ -123,13 +118,12 @@ for i,enemy in pairs(GoS:GetEnemyHeroes()) do
 		
 -- Start Timers
 
-
-	-- if (global_ticks1 + 0) < Ticker1 then
 	if (global_ticks1 + 10) < Ticker1 and GetCurrentHP(enemy) < dmg(enemy) then
 	
 		GoS:DelayAction( function ()
 			if not IsVisible(enemy) then
 				-- PrintChat("Start!")
+				enemyPos1 = GetOrigin(enemy)
 				TickerStart = GetTickCount()
 			end
 			if IsVisible(enemy) and GotBuff(enemy,"recall") == 1 then
@@ -150,11 +144,11 @@ OnProcessRecall(function(Object,recallProc)
 
 if recallProc.isStart == true then
 
-if CanUseSpell(myHero,_R) == READY and GetTeam(Object) ~= GetTeam(myHero) and GetCurrentHP(Object)+GetHPRegen(Object)*3 < dmg(Object) and mainMenu.recallult.recallult:Value() then
+if CanUseSpell(myHero,_R) == READY and GetTeam(Object) ~= GetTeam(myHero) and GetCurrentHP(Object)+GetHPRegen(Object)*3 < dmg(Object) and recallMenu.recallult.recallult1:Value() then
 
 enemyPos1 = GetOrigin(Object)
 
-if enemyPos2 ~= nil then
+if enemyPos2 ~= nil and enemyPos1 ~= nil then
 x1 = enemyPos2.x - enemyPos1.x
 y1 = enemyPos2.y - enemyPos1.y
 z1 = enemyPos2.z - enemyPos1.z
@@ -164,11 +158,11 @@ z1 = enemyPos2.z - enemyPos1.z
 		s = TickerEnd - TickerStart
 		ssec = s / 1000
 	
-	PrintChat("Sec: "..ssec)
+	-- PrintChat("Sec: "..ssec)
 	
 	if ssec < 3 then
 		PrintChat("RecallUlt on "..GetObjectName(Object))
-	elseif ssec > 3 and mainMenu.baseult.baseult:Value() then
+	elseif ssec > 3 and recallMenu.baseult.baseult1:Value() and GoS:GetDistance(enemyBasePos) <= skillrange then
 		PrintChat("BaseUlt on "..GetObjectName(Object))
 	end
 	
@@ -204,16 +198,14 @@ OnProcessRecall(function(Object,recallProc)
 
 if recallProc.isStart == true then
 
-	if CanUseSpell(myHero,_R) == READY and GetTeam(Object) ~= GetTeam(myHero) and GetCurrentHP(Object)+GetHPRegen(Object)*8 < dmg(Object) and mainMenu.baseult.baseult:Value() then
-	-- if GetTeam(Object) ~= GetTeam(myHero) and GetCurrentHP(Object) < dmg(Object) and mainMenu.RecallUlt.baseult:Value() then
+	if CanUseSpell(myHero,_R) == READY and GetTeam(Object) ~= GetTeam(myHero) and GetCurrentHP(Object)+GetHPRegen(Object)*8 < dmg(Object) and recallMenu.baseult.baseult1:Value() then
 
 	local t = recallProc.totalTime	
 	local myHeroPos = GetOrigin(myHero)
 	
 	tSpawn = ((GoS:GetDistance(enemyBasePos) / speedSpawn) + (delay/1000)) * 1000
 	distanceSpawn = GoS:GetDistance(enemyBasePos)
-	-- PrintChat("t: "..t)
-	-- PrintChat("tSpawn: "..tSpawn)
+
 	tt = t - tSpawn
 
 		if (tSpawn+delay) < t and distanceSpawn <= skillrange then
