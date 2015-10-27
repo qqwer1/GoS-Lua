a4cb5b16457a1a7a52543e5f178f15d9ddf08fb5
qqwer1 <cfz8points@yahoo.de>
2015-10-27 11:27:14 +0100

    Update Ezreal.lua

--- Ezreal.lua
+++ Ezreal.lua
@@ -1,5 +1,7 @@
 if GetObjectName(GetMyHero()) ~= "Ezreal" then return end
 
+if not pcall( require, "Inspired" ) then PrintChat("You are missing Inspired.lua!") return end
+
 PrintChat("ADC MAIN | Ezreal loaded.")
 PrintChat("by Noddy")
 
