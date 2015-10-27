d20f8f1c5621d1dd23ea1b998e4eecd875c4399d
qqwer1 <cfz8points@yahoo.de>
2015-10-27 11:31:05 +0100

    Update Caitlyn.lua

--- Caitlyn.lua
+++ Caitlyn.lua
@@ -1,34 +1,36 @@
-require('Inspired')
+if GetObjectName(GetMyHero()) ~= "Caitlyn" then return end
 
-mainMenu = Menu("ADC MAIN | Caitlyn", "Caitlyn")
-mainMenu:SubMenu("Combo", "Combo")
+if not pcall( require, "Inspired" ) then PrintChat("You are missing Inspired.lua!") return end
+
+local mainMenu = Menu("ADC MAIN | Caitlyn", "Caitlyn")
+mainMenu:Menu("Combo", "Combo")
 mainMenu.Combo:Boolean("useQ", "Use Q in combo", true)
 mainMenu.Combo:Boolean("useW", "Use W on close enemy", true)
 mainMenu.Combo:Boolean("useR", "Use R in combo", true)
 mainMenu.Combo:Key("Combo1", "Combo", string.byte(" "))
 ---------------------------------------------------------------------------------
-mainMenu:SubMenu("AutoW", "AutoW")
+mainMenu:Menu("AutoW", "AutoW")
 mainMenu.AutoW:Boolean("useWgap", "Use W on gapcloser", true)
 mainMenu.AutoW:Boolean("useWc", "Use W on chanelling spells", true)
 mainMenu.AutoW:Boolean("useWs", "Use W on immobile", true)
 ---------------------------------------------------------------------------------
-mainMenu:SubMenu("Harass", "Harass")
+mainMenu:Menu("Harass", "Harass")
 mainMenu.Harass:Boolean("hQ", "Use Q", true)
 mainMenu.Harass:Slider("Mana","Mana-Manager", 60, 1, 100, 1)
 mainMenu.Harass:Key("Harass1", "Harass", string.byte("C"))
 ---------------------------------------------------------------------------------
--- mainMenu:SubMenu("Killsteal", "Killsteal")
+-- mainMenu:Menu("Killsteal", "Killsteal")
 -- mainMenu.Killsteal:Boolean("ksQ", "Use Q - KS", true)
 -- mainMenu.Killsteal:Boolean("ksE", "Use E - KS", true)
 -- mainMenu.Killsteal:Boolean("ksR", "Use R - KS", true)
 ---------------------------------------------------------------------------------
-mainMenu:SubMenu("Items", "Items")
+mainMenu:Menu("Items", "Items")
 mainMenu.Items:Boolean("useCut", "Bilgewater Cutlass", true)
 mainMenu.Items:Boolean("useBork", "Blade of the Ruined King", true)
 mainMenu.Items:Boolean("useGhost", "Youmuu's Ghostblade", true)
 mainMenu.Items:Boolean("useRedPot", "Elixir of Wrath", true)
 ---------------------------------------------------------------------------------
-mainMenu:SubMenu("Misc", "Misc")
+mainMenu:Menu("Misc", "Misc")
 mainMenu.Misc:Boolean("drawR", "Draw R-Damage", true)
 mainMenu.Misc:Boolean("gapE", "Auto E agains gapcloser", true)
 mainMenu.Misc:Key("useEm", "Use E to mouse", string.byte("T"))
@@ -100,18 +102,18 @@ local Spell1g = GAPCLOSER_SPELLS[spell.name]
 	if Spell1g and spell.target == myHero and Spell1g.Name == GetObjectName(unit) and GetTeam(unit) ~= GetTeam(myHero) and GetObjectType(unit) == Obj_AI_Hero and CanUseSpell(myHero,_E) == READY then
 		local myHeroPos = GetOrigin(myHero)
 		local ePos1 = VectorWay(myHeroPos,GetOrigin(unit))
-		local ePosEnd1 = myHeroPos - (ePos1/GoS:GetDistance(myHeroPos, GetOrigin(unit)))*400	
-		if GoS:EnemiesAround(myHeroPos, 600) >= GoS:EnemiesAround(ePosEnd1, 500) then
+		local ePosEnd1 = myHeroPos - (ePos1/GetDistance(myHeroPos, GetOrigin(unit)))*400	
+		if EnemiesAround(myHeroPos, 600) >= EnemiesAround(ePosEnd1, 500) then
 			CastSkillShot(_E,GetOrigin(unit))
 		end
 	end
 local Spell2g = GAPCLOSER2_SPELLS[spell.name]
 	if Spell2g and Spell2g.Name == GetObjectName(unit) and GetTeam(unit) ~= GetTeam(myHero) and GetObjectType(unit) == Obj_AI_Hero and CanUseSpell(myHero,_E) == READY then
-		if GoS:GetDistance(spell.endPos, myHero) < 300 then
+		if GetDistance(spell.endPos, myHero) < 300 then
 			local myHeroPos = GetOrigin(myHero)
 			local ePos2 = VectorWay(myHeroPos,GetOrigin(unit))
-			local ePosEnd2 = myHeroPos - (ePos2/GoS:GetDistance(myHeroPos, GetOrigin(unit)))*400
-			if GoS:EnemiesAround(myHeroPos, 600) >= GoS:EnemiesAround(ePosEnd2, 500) then
+			local ePosEnd2 = myHeroPos - (ePos2/GetDistance(myHeroPos, GetOrigin(unit)))*400
+			if EnemiesAround(myHeroPos, 600) >= EnemiesAround(ePosEnd2, 500) then
 				CastSkillShot(_E,GetOrigin(unit))
 			end
 		end
@@ -122,13 +124,13 @@ end
 if mainMenu.AutoW.useWgap:Value() then
 local Spell1 = GAPCLOSER_SPELLS[spell.name]
 	if Spell1 and spell.target == myHero and Spell1.Name == GetObjectName(unit) and GetTeam(unit) ~= GetTeam(myHero) and GetObjectType(unit) == Obj_AI_Hero and CanUseSpell(myHero,_W) == READY then
-		GoS:DelayAction(function()
+		DelayAction(function()
 			CastSkillShot(_W,GetOrigin(myHero))
 		end, 250)
 	end
 local Spell2 = GAPCLOSER2_SPELLS[spell.name]
 	if Spell2 and Spell2.Name == GetObjectName(unit) and GetTeam(unit) ~= GetTeam(myHero) and GetObjectType(unit) == Obj_AI_Hero and CanUseSpell(myHero,_W) == READY then
-		if GoS:GetDistance(spell.endPos, myHero) < 250 then
+		if GetDistance(spell.endPos, myHero) < 250 then
 			CastSkillShot(_W,spell.endPos)
 		end
 	end
@@ -146,27 +148,27 @@ end)
 OnUpdateBuff(function(unit, buff)
 if mainMenu.AutoW.useWs:Value() then
 	if GetTeam(unit) ~= GetTeam(myHero) and GetObjectType(unit) == Obj_AI_Hero then
-		if buff.Type == 11  and GoS:IsInDistance(unit, 825+GetHitBox(unit)) then
+		if buff.Type == 11  and IsInDistance(unit, 825+GetHitBox(unit)) then
 			snaredtarget = unit
 			snared = true
 		end
-		if buff.Type == 5  and GoS:IsInDistance(unit, 825+GetHitBox(unit)) then
+		if buff.Type == 5  and IsInDistance(unit, 825+GetHitBox(unit)) then
 			stunedtarget = unit
 			stuned = true
 		end
-		if buff.Type == 29  and GoS:IsInDistance(unit, 800+GetHitBox(unit)) then
+		if buff.Type == 29  and IsInDistance(unit, 800+GetHitBox(unit)) then
 			uptarget = unit
 			up = true
 		end
-		if buff.Type == 28  and GoS:IsInDistance(unit, 850+GetHitBox(unit)) then
+		if buff.Type == 28  and IsInDistance(unit, 850+GetHitBox(unit)) then
 			fleetarget = unit
 			flee = true
 		end
-		if buff.Type == 8  and GoS:IsInDistance(unit, 850+GetHitBox(unit)) then
+		if buff.Type == 8  and IsInDistance(unit, 850+GetHitBox(unit)) then
 			taunttarget = unit
 			taunt = true
 		end
-		if buff.Type == 22  and GoS:IsInDistance(unit, 825+GetHitBox(unit)) then
+		if buff.Type == 22  and IsInDistance(unit, 825+GetHitBox(unit)) then
 			charmtarget = unit
 			charm = true
 		end
@@ -177,23 +179,34 @@ end)
 OnRemoveBuff(function(unit, buff)
 if mainMenu.AutoW.useWs:Value() then
 	if GetTeam(unit) ~= GetTeam(myHero) and GetObjectType(unit) == Obj_AI_Hero then
-		if buff.Type == 11  and GoS:IsInDistance(unit, 800+GetHitBox(unit)+1000) then
+		if buff.Type == 11  and IsInDistance(unit, 800+GetHitBox(unit)+1000) then
 			snared = false
 		end
-		if buff.Type == 5  and GoS:IsInDistance(unit, 800+GetHitBox(unit)+1000) then
+		if buff.Type == 5  and IsInDistance(unit, 800+GetHitBox(unit)+1000) then
 			stuned = false
 		end
-		if buff.Type == 29  and GoS:IsInDistance(unit, 800+GetHitBox(unit)+1000) then
+		if buff.Type == 29  and IsInDistance(unit, 800+GetHitBox(unit)+1000) then
 			up = false
 		end
-		if buff.Type == 28  and GoS:IsInDistance(unit, 800+GetHitBox(unit)+1000) then
+		if buff.Type == 28  and IsInDistance(unit, 800+GetHitBox(unit)+1000) then
 			flee = false
 		end
-		if buff.Type == 8  and GoS:IsInDistance(unit, 800+GetHitBox(unit)+1000) then
+		if buff.Type == 8  and IsInDistance(unit, 800+GetHitBox(unit)+1000) then
 			taunt = false
 		end
-		if buff.Type == 22  and GoS:IsInDistance(unit, 800+GetHitBox(unit)+1000) then
+		if buff.Type == 22  and IsInDistance(unit, 800+GetHitBox(unit)+1000) then
 			charm = false
+		end
+	end
+end
+end)
+
+OnDraw(function(myHero)
+-- Draw R Damage
+if mainMenu.Misc.drawR:Value() and CanUseSpell(myHero,_R) == READY then
+	for i,enemy in pairs(GetEnemyHeroes()) do
+		if ValidTarget(enemy, 500*GetCastLevel(myHero,_R)+1500) then
+			DrawDmgOverHpBar(enemy,GetCurrentHP(enemy),CalcDamage(myHero, enemy, 225*GetCastLevel(myHero,_R)+25+2*GetBonusDmg(myHero),0),0,0xff00ff00)
 		end
 	end
 end
@@ -210,33 +223,33 @@ local ghost = GetItemSlot(myHero,3142)
 local redpot = GetItemSlot(myHero,2140)
 
 if mainMenu.AutoW.useWs:Value() then
-	if snared ~= nil and snared == true and GoS:IsInDistance(snaredtarget, 800) then
+	if snared ~= nil and snared == true and IsInDistance(snaredtarget, 800) then
 		CastSkillShot(_W, GetOrigin(snaredtarget))
 	end
-	if stuned ~= nil and stuned == true and GoS:IsInDistance(stunedtarget, 800) then
+	if stuned ~= nil and stuned == true and IsInDistance(stunedtarget, 800) then
 		CastSkillShot(_W, GetOrigin(stunedtarget))
 	end
-	if up ~= nil and up == true and GoS:IsInDistance(uptarget, 800) then
+	if up ~= nil and up == true and IsInDistance(uptarget, 800) then
 		CastSkillShot(_W, GetOrigin(uptarget))
 	end
-	if flee ~= nil and flee == true and GoS:IsInDistance(fleetarget, 800) then
-		GoS:DelayAction(function()
+	if flee ~= nil and flee == true and IsInDistance(fleetarget, 800) then
+		DelayAction(function()
 		local fleeWPred = GetPredictionForPlayer(myHeroPos,fleetarget,GetMoveSpeed(fleetarget),math.huge, 1500, 800, 70, false, true)
 			if fleeWPred.HitChance == 1 then
 				CastSkillShot(_W, fleeWPred.PredPos.x, fleeWPred.PredPos.y, fleeWPred.PredPos.z)
 			end
 		end,100)
 	end
-	if taunt ~= nil and taunt == true and GoS:IsInDistance(taunttarget, 800) then
-		GoS:DelayAction(function()
+	if taunt ~= nil and taunt == true and IsInDistance(taunttarget, 800) then
+		DelayAction(function()
 		local tauntWPred = GetPredictionForPlayer(myHeroPos,taunttarget,GetMoveSpeed(taunttarget),math.huge, 1500, 800, 70, false, true)
 			if tauntWPred.HitChance == 1 then
 				CastSkillShot(_W, tauntWPred.PredPos.x, tauntWPred.PredPos.y, tauntWPred.PredPos.z)
 			end
 		end,100)
 	end
-	if charm ~= nil and charm == true and GoS:IsInDistance(charmtarget, 800) then
-		GoS:DelayAction(function()
+	if charm ~= nil and charm == true and IsInDistance(charmtarget, 800) then
+		DelayAction(function()
 		local charmWPred = GetPredictionForPlayer(myHeroPos,charmtarget,GetMoveSpeed(charmtarget),math.huge, 1500, 800, 70, false, true)
 			if charmWPred.HitChance == 1 then
 				CastSkillShot(_W, charmWPred.PredPos.x, charmWPred.PredPos.y, charmWPred.PredPos.z)
@@ -256,41 +269,41 @@ end
 if mainMenu.Combo.Combo1:Value() then
 
 -- Items
-	if CutBlade >= 1 and GoS:ValidTarget(target,550+50) and mainMenu.Items.useCut:Value() then
+	if CutBlade >= 1 and ValidTarget(target,550+50) and mainMenu.Items.useCut:Value() then
 		if CanUseSpell(myHero,GetItemSlot(myHero,3144)) == READY then
 			CastTargetSpell(target, GetItemSlot(myHero,3144))
 		end	
-	elseif bork >= 1 and GoS:ValidTarget(target,550+50) and (GetMaxHP(myHero) / GetCurrentHP(myHero)) >= 1.25 and mainMenu.Items.useBork:Value() then 
+	elseif bork >= 1 and ValidTarget(target,550+50) and (GetMaxHP(myHero) / GetCurrentHP(myHero)) >= 1.25 and mainMenu.Items.useBork:Value() then 
 		if CanUseSpell(myHero,GetItemSlot(myHero,3153)) == READY then
 			CastTargetSpell(target,GetItemSlot(myHero,3153))
 		end
 	end
 
-	if ghost >= 1 and GoS:ValidTarget(target,GetRange(myHero)+50) and mainMenu.Items.useGhost:Value() then
+	if ghost >= 1 and ValidTarget(target,GetRange(myHero)+50) and mainMenu.Items.useGhost:Value() then
 		if CanUseSpell(myHero,GetItemSlot(myHero,3142)) == READY then
 			CastSpell(GetItemSlot(myHero,3142))
 		end
 	end
 	
-	if redpot >= 1 and GoS:ValidTarget(target,GetRange(myHero)+50) and mainMenu.Items.useRedPot:Value() then
+	if redpot >= 1 and ValidTarget(target,GetRange(myHero)+50) and mainMenu.Items.useRedPot:Value() then
 		if CanUseSpell(myHero,GetItemSlot(myHero,2140)) == READY then
 			CastSpell(GetItemSlot(myHero,2140))
 		end
 	end
 --
-if mainMenu.Combo.useW:Value() and GoS:ValidTarget(target, 500) and CanUseSpell(myHero,_W) == READY then
+if mainMenu.Combo.useW:Value() and ValidTarget(target, 500) and CanUseSpell(myHero,_W) == READY then
 	local WPred = GetPredictionForPlayer(myHeroPos,target,GetMoveSpeed(target),math.huge, 1500, 800, 70, false, true)
-	if WPred.HitChance == 1 and GoS:GetDistance(myHeroPos, WPred.PredPos) < 250+GetHitBox(target) then
+	if WPred.HitChance == 1 and GetDistance(myHeroPos, WPred.PredPos) < 250+GetHitBox(target) then
 		CastSkillShot(_W, WPred.PredPos.x, WPred.PredPos.y, WPred.PredPos.z)
 	end
 end
 
-if mainMenu.Combo.useQ:Value() and GoS:ValidTarget(target, 1300) and CanUseSpell(myHero,_Q) == READY then
-	local AA = (GoS:CalcDamage(myHero,target,GetBaseDamage(myHero)+GetBonusDmg(myHero),0) * (baseATKSpeed*GetAttackSpeed(myHero)))
+if mainMenu.Combo.useQ:Value() and ValidTarget(target, 1300) and CanUseSpell(myHero,_Q) == READY then
+	local AA = (CalcDamage(myHero,target,GetBaseDamage(myHero)+GetBonusDmg(myHero),0) * (baseATKSpeed*GetAttackSpeed(myHero)))
 	local AACrit = (2*AA) * GetCritChance(myHero)
-	local qDMG = GoS:CalcDamage(myHero,target,40*GetCastLevel(myHero,_Q)-20+1.3*(GetBaseDamage(myHero)+GetBonusDmg(myHero)),0)
-	local qDMG2 = GoS:CalcDamage(myHero,target,20*GetCastLevel(myHero,_Q)-10+0.65*(GetBaseDamage(myHero)+GetBonusDmg(myHero)),0)
-		if GoS:ValidTarget(target,650) then
+	local qDMG = CalcDamage(myHero,target,40*GetCastLevel(myHero,_Q)-20+1.3*(GetBaseDamage(myHero)+GetBonusDmg(myHero)),0)
+	local qDMG2 = CalcDamage(myHero,target,20*GetCastLevel(myHero,_Q)-10+0.65*(GetBaseDamage(myHero)+GetBonusDmg(myHero)),0)
+		if ValidTarget(target,650) then
 			local QPred = GetPredictionForPlayer(myHeroPos,target,GetMoveSpeed(target),2200, 625, 1300, 90, true, false)
 			local QPred2 = GetPredictionForPlayer(myHeroPos,target,GetMoveSpeed(target),2200, 625, 1300, 90, false, false)
 				if QPred.HitChance == 1 and qDMG > AA+AACrit then
@@ -299,7 +312,7 @@ if mainMenu.Combo.useQ:Value() and GoS:ValidTarget(target, 1300) and CanUseSpell
 				if QPred.HitChance == 0 and QPred2.HitChance == 1 and qDMG2 > AA+AACrit then 
 					CastSkillShot(_Q, QPred2.PredPos.x, QPred2.PredPos.y, QPred2.PredPos.z)
 				end
-		elseif GoS:ValidTarget(target, 1300) and not GoS:IsInDistance(target, 650+GetHitBox(target)) then
+		elseif ValidTarget(target, 1300) and not IsInDistance(target, 650+GetHitBox(target)) then
 			local QPred2 = GetPredictionForPlayer(myHeroPos,target,GetMoveSpeed(target),2200, 625, 1300, 90, false, false)
 				if QPred2.HitChance == 1 then
 					CastSkillShot(_Q, QPred2.PredPos.x, QPred2.PredPos.y, QPred2.PredPos.z)
@@ -308,8 +321,8 @@ if mainMenu.Combo.useQ:Value() and GoS:ValidTarget(target, 1300) and CanUseSpell
 end
 
 if mainMenu.Combo.useR:Value() and CanUseSpell(myHero,_R) == READY then
-	for i,enemy in pairs(GoS:GetEnemyHeroes()) do
-		if GoS:ValidTarget(enemy, 500*GetCastLevel(myHero,_R)+1500) and GoS:EnemiesAround(myHeroPos, 1100) == 0 and not GoS:IsInDistance(enemy, 1100) and GetCurrentHP(enemy) + GetDmgShield(enemy) + GetHPRegen(enemy)*2 < GoS:CalcDamage(myHero, enemy, 225*GetCastLevel(myHero,_R)+25+2*GetBonusDmg(myHero),0) then
+	for i,enemy in pairs(GetEnemyHeroes()) do
+		if ValidTarget(enemy, 500*GetCastLevel(myHero,_R)+1500) and EnemiesAround(myHeroPos, 1100) == 0 and not IsInDistance(enemy, 1100) and GetCurrentHP(enemy) + GetDmgShield(enemy) + GetHPRegen(enemy)*2 < CalcDamage(myHero, enemy, 225*GetCastLevel(myHero,_R)+25+2*GetBonusDmg(myHero),0) then
 			CastTargetSpell(enemy,_R)
 		end
 	end
@@ -318,19 +331,10 @@ end
 end -- Combo
 
 -- Harass
-if mainMenu.Harass.Harass1:Value() and CanUseSpell(myHero,_Q) == READY and 100*GetCurrentMana(myHero)/GetMaxMana(myHero) >= mainMenu.Harass.Mana:Value() and GoS:ValidTarget(target, 1300) then
+if mainMenu.Harass.Harass1:Value() and CanUseSpell(myHero,_Q) == READY and 100*GetCurrentMana(myHero)/GetMaxMana(myHero) >= mainMenu.Harass.Mana:Value() and ValidTarget(target, 1300) then
 	local QPred = GetPredictionForPlayer(myHeroPos,target,GetMoveSpeed(target),2200, 625, 1300, 90, true, false)
 	if QPred.HitChance == 1 then
 		CastSkillShot(_Q, QPred.PredPos.x, QPred.PredPos.y, QPred.PredPos.z)
-	end
-end
-
--- Draw R Damage
-if mainMenu.Misc.drawR:Value() and CanUseSpell(myHero,_R) == READY then
-	for i,enemy in pairs(GoS:GetEnemyHeroes()) do
-		if GoS:ValidTarget(enemy, 500*GetCastLevel(myHero,_R)+1500) then
-			DrawDmgOverHpBar(enemy,GetCurrentHP(enemy),GoS:CalcDamage(myHero, enemy, 225*GetCastLevel(myHero,_R)+25+2*GetBonusDmg(myHero),0),0,0xff00ff00)
-		end
 	end
 end
 
