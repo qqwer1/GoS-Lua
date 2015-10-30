aaab1bac37abfde7648c107f1bfe6094da2ad82c
qqwer1 <cfz8points@yahoo.de>
2015-10-30 22:22:08 +0100

    Update RecallUlt.lua

--- RecallUlt.lua
+++ RecallUlt.lua
@@ -1,12 +1,66 @@
-require('Inspired')
+if not pcall( require, "Inspired" ) then PrintChat("You are missing Inspired.lua!") return end
 
-recallMenu = Menu("RecallUlt", "RecallUlt1")
-recallMenu:SubMenu("recallult", "Recall Ult (Beta)")
+-- Champ List 
+local supportedChamp = {
+["Ashe"] = {
+skillrange = 50000,
+speedChamp = 1600,
+delay = 250,
+colision = true,
+dmg = function(target) return CalcDamage(myHero, target, 0, 175*GetCastLevel(myHero,_R)+ 75 + GetBonusAP(myHero)) end
+},
+["Draven"] = {
+skillrange = 50000,
+speedChamp = 2000,
+delay = 400,
+colision = false,
+dmg = function(target) return CalcDamage(myHero, target, 0, 60*GetCastLevel(myHero,_R)+ 30 + (0.44*GetBonusDmg(myHero)+GetBaseDamage(myHero))) end
+},
+["Ezreal"] = {
+skillrange = 50000,
+speedChamp = 2000,
+delay = 1000,
+colision = false,
+-- dmg = function(target) return CalcDamage(myHero, target, 0, (150*GetCastLevel(myHero,_R)+ 40 + (0.80*GetBonusAP(myHero)) + (0.90*(GetBaseDamage(myHero) + GetBonusDmg(myHero))))) end
+dmg = function(target) return CalcDamage(myHero, target, 0, 45*GetCastLevel(myHero,_R)+ 60 + (0.27*GetBonusAP(myHero)) + (0.90*(GetBaseDamage(myHero) + GetBonusDmg(myHero)))) end
+},
+["Jinx"] = {
+skillrange = 50000,
+speedChamp = 2000,
+-- speedSpawn = (GetDistance(enemyBasePos) / (1 + (GetDistance(enemyBasePos)-1700)/2600)),
+delay = 600,
+colision = true,
+dmg = function(target) return CalcDamage(myHero, target, ((GetMaxHP(target)-GetCurrentHP(target))*(0.2+0.05*GetCastLevel(myHero, _R))) + 100*GetCastLevel(myHero,_R) + 150 + GetBonusDmg(myHero)) end
+},
+["Gangplank"] = {
+skillrange = 50000,
+speedChamp = math.huge,
+delay = 0,
+colision = false,
+dmg = function(target) return CalcDamage(myHero, target, 0, 20*GetCastLevel(myHero,_R)+ 30 + 0.1*GetBonusAP(myHero)) end
+},
+["Lux"] = {
+skillrange = 3340,
+speedChamp = math.huge,
+delay = 500,
+colision = false,
+dmg = function(target) return CalcDamage(myHero, target, 0, 100*GetCastLevel(myHero,_R)+ 200 + 0.75*GetBonusAP(myHero)) end
+},
+["Lux"] = {
+skillrange = 5400,
+speedChamp = 5400/3.5,
+delay = 0,
+colision = false,
+dmg = function(target) return CalcDamage(myHero, target, 0, 100*GetCastLevel(myHero,_R)+ 100 + 0.72*GetBonusAP(myHero)) end
+},
+}
+
+if not supportedChamp[GetObjectName(myHero)] then return end
+
+local recallMenu = Menu("RecallUlt", "RecallUlt1")
+recallMenu:Menu("recallult", "Recall Ult (Beta)")
 recallMenu.recallult:Boolean("recallult1", "Recall Ult (Beta)", true)
 recallMenu.recallult:Boolean("recalldraw", "Draw Ult Pos", true)
-recallMenu:SubMenu("baseult", "BaseUlt")
-recallMenu.baseult:Boolean("baseult1", "Base Ult", true)
-
 
 PrintChat("Noddy | RecallUlt loaded.")
 
@@ -14,95 +68,32 @@ global_ticks = 0
 global_ticks1 = 0
 TickerStart = 0
 
-local myHero = GetMyHero()
-
--- Team
-if GetTeam(myHero) == 100 then 
-	enemyBasePos = Vector(14340, 171, 14390)
-elseif GetTeam(myHero) == 200 then 
-	enemyBasePos = Vector(400, 200, 400)
-end
-
-
--- Champ List 
---Ashe
-if GetObjectName(myHero) == "Ashe" then 
-skillrange = 50000
-speedChamp = 1600
-speedSpawn = 1600
-delay = 250
-colision = true
-dmg = function(target) return GoS:CalcDamage(myHero, target, 0, 175*GetCastLevel(myHero,_R)+ 75 + GetBonusAP(myHero)) end
---Draven
-elseif GetObjectName(myHero) == "Draven" then
-skillrange = 50000
-speedChamp = 2000
-speedSpawn = 2000
-delay = 400
-colision = false
-dmg = function(target) return GoS:CalcDamage(myHero, target, 0, 60*GetCastLevel(myHero,_R)+ 30 + (0.44*GetBonusDmg(myHero)+GetBaseDamage(myHero))) end
---Ezreal
-elseif GetObjectName(myHero) == "Ezreal" then
-skillrange = 50000
-speedChamp = 2000
-speedSpawn = 2000
-delay = 1000
-colision = false
--- dmg = function(target) return GoS:CalcDamage(myHero, target, 0, (150*GetCastLevel(myHero,_R)+ 40 + (0.80*GetBonusAP(myHero)) + (0.90*(GetBaseDamage(myHero) + GetBonusDmg(myHero))))) end
-dmg = function(target) return GoS:CalcDamage(myHero, target, 0, 45*GetCastLevel(myHero,_R)+ 60 + (0.27*GetBonusAP(myHero)) + (0.90*(GetBaseDamage(myHero) + GetBonusDmg(myHero)))) end
---Jinx
-elseif GetObjectName(myHero) == "Jinx" then 
-skillrange = 50000
-speedChamp = 2000
-speedSpawn = (GoS:GetDistance(enemyBasePos) / (1 + (GoS:GetDistance(enemyBasePos)-1700)/2600))
-delay = 600
-colision = true
-dmg = function(target) return GoS:CalcDamage(myHero, target, ((GetMaxHP(target)-GetCurrentHP(target))*(0.2+0.05*GetCastLevel(myHero, _R))) + 100*GetCastLevel(myHero,_R) + 150 + GetBonusDmg(myHero)) end
---Gangplank
-elseif GetObjectName(myHero) == "Gangplank" then
-skillrange = 50000
-speedChamp = 500000
-speedSpawn = 500000
-delay = 0
-colision = false
-dmg = function(target) return GoS:CalcDamage(myHero, target, 0, 20*GetCastLevel(myHero,_R)+ 30 + 0.1*GetBonusAP(myHero)) end
---Lux
-elseif GetObjectName(myHero) == "Lux" then
-skillrange = 3340
-speedChamp = 33400
-speedSpawn = 33400
-delay = 500
-colision = false
-dmg = function(target) return GoS:CalcDamage(myHero, target, 0, 100*GetCastLevel(myHero,_R)+ 200 + 0.75*GetBonusAP(myHero)) end
---Ziggs
-elseif GetObjectName(myHero) == "Ziggs" then
-skillrange = 5400
-speedChamp = skillrange/3.5
-speedSpawn = skillrange/3.5
-delay = 0
-colision = false
-dmg = function(target) return GoS:CalcDamage(myHero, target, 0, 100*GetCastLevel(myHero,_R)+ 100 + 0.72*GetBonusAP(myHero)) end
-end
+local skillrange = supportedChamp[GetObjectName(myHero)].skillrange
+local speedChamp = supportedChamp[GetObjectName(myHero)].speedChamp
+local delay = supportedChamp[GetObjectName(myHero)].delay
+local dmg = supportedChamp[GetObjectName(myHero)].dmg
 
 -- RECALL ULT
-OnLoop (function (myHero)
+OnTick (function (myHero)
 
 Ticker1 = GetTickCount()
 
 if EndPos ~= nil and recallMenu.recallult.recalldraw:Value() then
-DrawCircle(EndPos,25,1,100,0xff00ffff)
-	GoS:DelayAction(function ()	
+DrawCircle(EndPos,25,1,255,0xff00ffff)
+DrawCircle(enemyPos1,circlerange,1,100,0xff00ffff)
+	DelayAction(function ()	
 		EndPos = nil	
 	end, 8000)
 end
 
-for i,enemy in pairs(GoS:GetEnemyHeroes()) do	
-	if CanUseSpell(myHero,_R) == READY and recallMenu.recallult.recallult1:Value() and GoS:ValidTarget(enemy) and GetCurrentHP(enemy) < dmg(enemy) then
+for i,enemy in pairs(GetEnemyHeroes()) do	
+
+	if CanUseSpell(myHero,_R) == READY and recallMenu.recallult.recallult1:Value() and ValidTarget(enemy) and GetCurrentHP(enemy) < dmg(enemy) then
 
 		Ticker = GetTickCount()
 		
 		if (global_ticks + 1000) < Ticker then
-		GoS:DelayAction( function ()
+		DelayAction( function ()
 		
 			if IsVisible(enemy) then
 			enemyPos2 = GetOrigin(enemy)
@@ -120,7 +111,7 @@ for i,enemy in pairs(GoS:GetEnemyHeroes()) do
 
 	if (global_ticks1 + 10) < Ticker1 and GetCurrentHP(enemy) < dmg(enemy) then
 	
-		GoS:DelayAction( function ()
+		DelayAction( function ()
 			if not IsVisible(enemy) then
 				-- PrintChat("Start!")
 				enemyPos1 = GetOrigin(enemy)
@@ -144,77 +135,49 @@ OnProcessRecall(function(Object,recallProc)
 
 if recallProc.isStart == true then
 
-if CanUseSpell(myHero,_R) == READY and GetTeam(Object) ~= GetTeam(myHero) and GetCurrentHP(Object)+GetHPRegen(Object)*3 < dmg(Object) and recallMenu.recallult.recallult1:Value() then
+if CanUseSpell(myHero,_R) == READY and GetTeam(Object) ~= GetTeam(myHero) and GetObjectType(Object) == GetObjectType(myHero) and GetCurrentHP(Object)+GetHPRegen(Object)*3 < dmg(Object) and recallMenu.recallult.recallult1:Value() then
 
 enemyPos1 = GetOrigin(Object)
 
 if enemyPos2 ~= nil and enemyPos1 ~= nil then
-x1 = enemyPos2.x - enemyPos1.x
-y1 = enemyPos2.y - enemyPos1.y
-z1 = enemyPos2.z - enemyPos1.z
 
+local x1 = enemyPos2.x - enemyPos1.x
+local y1 = enemyPos2.y - enemyPos1.y
+local z1 = enemyPos2.z - enemyPos1.z
 
-		TickerEnd = GetTickCount()
-		s = TickerEnd - TickerStart
-		ssec = s / 1000
+		local TickerEnd = GetTickCount()
+		local s = TickerEnd - TickerStart
+		local ssec = s / 1000
 	
 	-- PrintChat("Sec: "..ssec)
-	
-	if ssec < 3 then
-		PrintChat("RecallUlt on "..GetObjectName(Object))
-	elseif ssec > 3 and recallMenu.baseult.baseult1:Value() and GoS:GetDistance(enemyBasePos) <= skillrange then
-		PrintChat("BaseUlt on "..GetObjectName(Object))
-	end
 	
 	local t = recallProc.totalTime
 
 	local myHeroPos = GetOrigin(myHero)
 	local targetPos = GetOrigin(Object)
+	circlerange = GetMoveSpeed(Object)*ssec
 	
-		EndPosX = targetPos.x + (-ssec*(x1))
-		EndPosY = targetPos.y + (-ssec*(y1))
-		EndPosZ = targetPos.z + (-ssec*(z1))
+		local EndPosX = targetPos.x + (-ssec*(x1))
+		local EndPosY = targetPos.y + (-ssec*(y1))
+		local EndPosZ = targetPos.z + (-ssec*(z1))
 
-		 EndPos = Vector(EndPosX,EndPosY,EndPosZ)
+		EndPos = Vector(EndPosX,EndPosY,EndPosZ)
 		
-	distanceChamp = math.sqrt(math.pow((myHeroPos.x - EndPosX),2) + math.pow((myHeroPos.y - EndPosY),2) + math.pow((myHeroPos.z - EndPosZ),2))
+	local distanceChamp = math.sqrt(math.pow((myHeroPos.x - EndPosX),2) + math.pow((myHeroPos.y - EndPosY),2) + math.pow((myHeroPos.z - EndPosZ),2))
 
 	tChamp = ((distanceChamp / speedChamp) + (delay/1000)) * 1000
-	
+
+	if GetDistance(targetPos,EndPos) < GetMoveSpeed(Object)*ssec then
 	
 		if tChamp < t and ssec < 3 and distanceChamp <= skillrange then
 
-			-- PrintChat("RecallUlt on "..GetObjectName(Object))
+			PrintChat("RecallUlt on "..GetObjectName(Object))
 			CastSkillShot(_R, EndPosX, EndPosY, EndPosZ)
 			enemyPos2 = nil
 		end
+	end
+	
 end		
 end
-end	
-end)
-
--- BASE ULT --------------------
-OnProcessRecall(function(Object,recallProc)
-
-if recallProc.isStart == true then
-
-	if CanUseSpell(myHero,_R) == READY and GetTeam(Object) ~= GetTeam(myHero) and GetCurrentHP(Object)+GetHPRegen(Object)*8 < dmg(Object) and recallMenu.baseult.baseult1:Value() then
-
-	local t = recallProc.totalTime	
-	local myHeroPos = GetOrigin(myHero)
-	
-	tSpawn = ((GoS:GetDistance(enemyBasePos) / speedSpawn) + (delay/1000)) * 1000
-	distanceSpawn = GoS:GetDistance(enemyBasePos)
-
-	tt = t - tSpawn
-
-		if (tSpawn+delay) < t and distanceSpawn <= skillrange then
-					-- PrintChat("BaseUlt on "..GetObjectName(Object))
-					GoS:DelayAction( function()
-						CastSkillShot(_R,enemyBasePos.x,enemyBasePos.y,enemyBasePos.z) end
-						, tt)
-		end
-	
-end	
 end	
 end)
