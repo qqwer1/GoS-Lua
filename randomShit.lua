142941c50f46384d5c756c1b62d6def03030eb18
qqwer1 <cfz8points@yahoo.de>
2016-05-22 22:01:05 +0200

    Create randomShit.lua

---
+++ randomShit.lua
@@ -0,0 +1,9 @@
+-- KappaMon
+-- 1 = Creep
+-- 2 = SmallMob
+-- 3 = BigMob
+-- 4 = Kill/Assist
+-- 5 = Drake_Element
+-- 6 = Rift/Baron/Ender
+-- 7 = Win
+local expGain = { [1] = 1, [2] = 1, [3] = 3, [4] = 5, [5] = 10, [6] = 25, [7] = 100}
