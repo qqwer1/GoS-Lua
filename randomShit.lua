dbde18255ec2e40d78e73ef92c5c01f32fbdda44
qqwer1 <cfz8points@yahoo.de>
2016-05-22 22:07:39 +0200

    Update randomShit.lua

--- randomShit.lua
+++ randomShit.lua
@@ -7,3 +7,7 @@
 -- 6 = Rift/Baron/Ender
 -- 7 = Win
 local expGain = { [1] = 1, [2] = 1, [3] = 3, [4] = 5, [5] = 10, [6] = 25, [7] = 100}
+
+--expNeeded
+local level = GetLevel(myKappa)
+local NeededExp = 100*level+(100*(0.5*level))
